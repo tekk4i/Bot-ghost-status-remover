d2c3703f6ebf04c5ca1efa1b1c7344b99d19d934
tekk4i <bulutkocak66@gmail.com>
2024-12-08 17:14:07 +0300

    index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["discord.gg/nitrotr", "🎬➡️💍➡️✍️➡️👑"];
+const statusMessages = ["ITC tarafından", "merci için yapıldı ❤️"];
 const statusTypes = ['online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
